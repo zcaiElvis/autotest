3caa7e7023df4216adad1c63a22095c65207e129
Elvis Cai <elvis@dhcp-128-189-221-179.ubcsecure.wireless.ubc.ca>
2023-01-31 16:24:28 -0800

    added runs for cv

--- config/exp3.R
+++ config/exp3.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(zoo)
 
 ### Specify Function to run
-fun <- estimate_rt
+fun <- cv_estimate_rt
 
 ### Handle owid data
 owid_trans <- function(owid) {
@@ -17,7 +17,7 @@ owid_trans <- function(owid) {
 # Reload data
 reload <- FALSE
 # Generate plot for summary
-gen_plot <- TRUE
+gen_plot <- FALSE
 
 ### Load Dataset ###
 if (reload) {
@@ -26,40 +26,29 @@ if (reload) {
   data_canada <- owid_trans(data_canada)
   data_canada <- na.fill(data_canada, "extend")
   data_canada <- list(observed_counts = data_canada[,1])
-  
+
   ### Toy data
   data_short <- list(observed_counts = c(1:10))
-  
+
   data_short_pad <- list(observed_counts = c(rep(0, 5), 1:10))
-  
+
   ### Synthetic data
   data_d2 <- list(observed_counts = read.csv("data/syn_d2.csv")$y)
-  
+
   ### Canadian Mpox Data
   data_mpox <- read.csv("data/owid-monkeypox-data.csv")
   data_mpox <- owid_trans(data_mpox)
   data_mpox <- list(observed_counts = data_mpox[,1])
 }
 
-data <- list(data_mpox, data_short, data_short_pad, data_d2, data_canada)
+data <- list(data_short)
 
 ### Set parameters
 
-# Most simple run
-p1 <- list(dist_gamma = c(2.5, 2.5),
-           degree = 2,
-           lambda = 1)
-# passed!
-
 # Multiple Lambda
-p2 <- list(dist_gamma = c(2.5, 2.5),
+p4 <- list(dist_gamma = c(2.5, 2.5),
            degree = 2,
-           lambda = c(1,2,3))
+           lambda = c(1:20),
+           fold = 3)
 
-# Only lambdamax and lambda_min_ratio
-p3 <- list(dist_gamma = c(2.5, 2.5),
-           degree = 2,
-           lambdamax = 5,
-           lambda_min_ratio = 0.1)
-
-params <- list(p1)
+params <- list(p4)
--- run.R
+++ run.R
@@ -8,28 +8,31 @@ dir.create(output_folder)
 
 
 ### Source the config file ###
-source("config/exp1.R")
-
+source("config/exp4.R")
 
 ### Loop through all configs
 output <- list()
 
 for (i in 1:length(data)) {
   for (j in 1:length(params)) {
-    
     ### Combine arguments ###
     args <- c(unlist(data[i], recursive = FALSE), unlist(params[j], recursive = FALSE))
-    print(args)
     ### Run code ###
     result <- do.call(fun, args=args)
     output <- append(output, result)
     ### Save case output ###
-    png(filename=paste0("log/", current_time, "/img_", i, j, ".png"), width=900, height=450)
-    plot(summary(result))
-    dev.off()
+    if (gen_plot) {
+      mod <- summary(result)
+      png(filename=paste0("log/", current_time, "/img_", i, j, ".png"), width=900, height=450)
+      plot(mod)
+      dev.off()
+    }
     ### Save data ####
     saveRDS(result, file = paste0("log/", current_time, "/", i, j, ".rds"))
-
+    writeLines(as.character(params[j]), 
+               paste0("log/", current_time, "/params", i, j, ".txt"))
+    writeLines(as.character(output), 
+               paste0("log/", current_time, "/results", i, j, ".txt"))
   }
 }
 
